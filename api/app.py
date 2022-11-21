e2ebb2a32604c81dac48c77d68da3545d36d44af
Vieira Neto <vieiranetoc@gmail.com>
2022-11-21 00:09:04 +0000

    add cors to flask app

--- api/app.py
+++ api/app.py
@@ -3,12 +3,14 @@ from flask import Flask
 import rosnode
 from dotenv import load_dotenv
 from os.path import join, dirname
+from flask_cors import CORS
 import ros_manager.manager as ros_manager
 
 dotenv_path = join(dirname(__file__), '.env')  # Path to .env file
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/list_nodes')
 def list_nodes():
@@ -20,5 +22,5 @@ def node_info(id):
     return ros_manager.node_info('/'+ id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', debug=True)
 
